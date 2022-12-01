9ea335bb3f46121d6358641d670f297857318001
DanRunfola <danr@wm.edu>
2022-12-01 10:09:28 -0500

    More on prem work - class based structure upgrades

--- geoBoundaryBuilder/build.py
+++ geoBoundaryBuilder/build.py
@@ -1,35 +1,27 @@
 import pandas as pd
-from dask.distributed import Client
-from dask_jobqueue import PBSCluster
 import os
 import time
+import mpi4py
+from mpi4py import MPI
+import itertools
+import math
+f = "/sciclone/home20/dsmillerrunfol/geoBoundaryBot/geoBoundaryBuilder/builderClass.py"
+exec(compile(open(f, "rb").read(), f, 'exec'))
 
+comm = MPI.COMM_WORLD
+comm_size = comm.Get_size()
+comm_rank = comm.Get_rank()
+print(comm_size)
+
+
+#Run Variables
 GB_DIR = "/sciclone/home20/dsmillerrunfol/geoBoundariesDev/geoBoundaries/"
 LOG_DIR = "/sciclone/home20/dsmillerrunfol/geoBoundaryBot/geoBoundaryBuilder/logs/"
 TMP_DIR = "/sciclone/home20/dsmillerrunfol/geoBoundariesTmp/"
 #Limits total number of ADM units & resources requested.
 TEST = True
 
-cluster_kwargs = {
-    "name": "gbBuild-workers",
-    "shebang": "#!/bin/tcsh",
-    "resource_spec": "nodes=1:vortex:ppn=12",
-    "walltime": "24:00:00",
-    "cores": 12,
-    "processes": 2,
-    "memory": "32GB",
-    "interface": "ib0",
-}
-
-cluster = PBSCluster(**cluster_kwargs)
-if(TEST == True):
-    cluster.scale(4)
-else:
-    cluster.scale(20)
-
-f = "/sciclone/home20/dsmillerrunfol/geoBoundaryBot/geoBoundaryBuilder/builderClass.py"
-exec(compile(open(f, "rb").read(), f, 'exec'))
-
+#===============
 admTypes = ["ADM0", "ADM1", "ADM2", "ADM3", "ADM4", "ADM5"]
 productTypes = ["geoBoundaries", "UN_SALB", "UN_OCHA"]
 #Load in ISOs from master ISO list
@@ -40,40 +32,56 @@ licenses = pd.read_csv("../dta/gbLicenses.csv")
 licenseList = licenses["license_name"].values
 
 if(TEST == True):
-    admTypes = ["ADM0"]
-    productTypes = ["gbOpen"]
-    isoList = ["USA", "MEX"]
+    admTypes = ["ADM0","ADM1","ADM2","ADM3"]
+    productTypes = ["geoBoundaries"]
+    isoList = ["USA", "MEX", "CAN", "FRA","AUS","BRA","UKR","NOR"]
 
-jobList = {}
-jobList["ADM"] = []
-jobList["product"] = []
-jobList["ISO"] = []
-for adm in admTypes:
-    for product in productTypes:
-        for iso in isoList:
-            jobList["ADM"].append(adm)
-            jobList["product"].append(product)
-            jobList["ISO"].append(iso)
+if(MPI.COMM_WORLD.Get_rank() == 0):
 
-print("Total Jobs: " + str(len(jobList["ADM"])))
+    jobList = []
+    for adm in admTypes:
+        for product in productTypes:
+            for iso in isoList:
+                jobList.append([iso, adm, product])
+                
 
-def build(ISO, ADM, product, validISO=isoList, validLicense=licenseList):
-    bnd = builder(ISO, ADM, product, GB_DIR, LOG_DIR, TMP_DIR, validISO, licenseList)
-    bnd.logger("\n\n\nLAYER BUILD COMMENCE TIMESTAMP", str(time.time()))   
+    print("Total Jobs: " + str(len(jobList)))
+
+    tasks_per_core = math.ceil(len(jobList)/ (comm_size-1))
+    print(tasks_per_core)
     
-    validSource = bnd.checkSourceValidity()
+    taskStart = 0
+    taskEnd = tasks_per_core
+    for core in range(1,(comm_size-1)):
+        chunk = jobList[taskStart:taskEnd]
+        comm.send(chunk, dest=core, tag=1)
+        taskStart = taskStart + tasks_per_core
+        taskEnd = taskEnd + tasks_per_core
 
-    outcome=validSource
-    return([ISO,ADM,product,outcome])
+else:
+    def build(ISO, ADM, product, validISO=isoList, validLicense=licenseList):
+        bnd = builder(ISO, ADM, product, GB_DIR, LOG_DIR, TMP_DIR, validISO, licenseList)
+        bnd.logger("\n\n\nLAYER BUILD COMMENCE TIMESTAMP", str(time.time()))   
 
-with Client(cluster) as client:
-    futures = client.map(build, jobList["ISO"], jobList["ADM"], jobList["product"])
-    result = [A.result() for A in futures]
-    print(result)
-    print("========")
-    print("========")
-    print("========")
-    print("SHUTDOWN")
-    print("========")
-    print("========")
-    print("========")
+        bnd.checkExistence()
+        if(bnd.existFail == 1):
+            return([ISO,ADM,product,"No source data for boundary exists.  Skipping."])
+            
+        validSource = bnd.checkSourceValidity()
+        if("ERROR" in validSource):
+            return([ISO,ADM,product,validSource])
+
+        metaBuild = bnd.buildTabularMetaData()
+        if("ERROR" in metaBuild):
+            return([ISO,ADM,product,metaBuild])
+        
+        return([ISO,ADM,product,"Succesfully built."])
+
+
+    layers = comm.recv(source=0, tag=1)
+    print("I (" + str(MPI.COMM_WORLD.Get_rank()) + ") have received " + str(layers) + " layers to build.")
+    ret = []
+    for l in layers:
+        ret.append(build(ISO=l[0], ADM=l[1], product=l[2]))
+    
+    print(ret)
--- geoBoundaryBuilder/builderClass.py
+++ geoBoundaryBuilder/builderClass.py
@@ -2,6 +2,9 @@ import zipfile
 import hashlib 
 import os
 import geopandas as gpd
+import datetime
+from shapely.validation import explain_validity
+from shapely.geometry import shape
 
 class builder:
     def __init__(self, ISO, ADM, product, basePath, logPath, tmpPath, validISO, validLicense):
@@ -12,9 +15,9 @@ class builder:
         self.logPath = logPath
         self.tmpPath = tmpPath
         self.sourcePath = self.basePath + "sourceData/" + self.product + "/" + self.ISO + "_" + self.ADM + ".zip"
-        self.zipExtractPath = self.tmpPath + + self.product + "/" + self.ISO + "_" + self.ADM + "/"
+        self.zipExtractPath = self.tmpPath + self.product + "/" + self.ISO + "_" + self.ADM + "/"
         self.validISO = validISO
-        self.validLicense = validLicense
+        self.validLicense = [x.lower().strip() for x in validLicense]        
 
         #Data Validity Checks
         self.existFail = 1
@@ -34,10 +37,18 @@ class builder:
         self.metaReq["license"] = "NONE"
         self.metaReq["licenseSource"] = "NONE"
         self.metaReq["dataSource"] = "NONE"
-        self.metaReq["canonical"] = "NONE"
+        self.metaReq["canonical"] = ""
         self.metaReq["licenseImage"] = "NONE"
-        self.metaReq["licenseNotes"] = "NONE"
-        
+        self.metaReq["licenseNotes"] = ""
+        self.metaReq["other notes"] = ""
+
+        #Geometry / Attribute Table Validity Checks
+        self.geomReq = {}
+        self.geomReq["projection"] = "NONE"
+        self.geomReq["names"] = "NONE"
+        self.geomReq["iso"] = "NONE"
+        self.geomReq["bounds"] = "NONE"
+        self.geomReq["valid"] = "NONE"
     
     def logger(self, type, message):
         with open(self.logPath + str(self.ISO)+str(self.ADM)+str(self.product)+".log", "a") as f:
@@ -84,7 +95,7 @@ class builder:
                 if(len(shp) == 1):
                     self.logger("INFO", "Shapefile (*.shp) found. Attempting to load.")
                     try:
-                        dta = gpd.read_file(self.zipExtractPath + shp[0])
+                        self.geomDta = gpd.read_file(self.zipExtractPath + shp[0])
                         self.dataLoadFail = 0
                     except Exception as e:
                         self.logger("CRITICAL", "The shape file provided failed to load. Make sure all required files are included (i.e., *.shx). Here is what I know: " + str(e))
@@ -92,7 +103,7 @@ class builder:
                 if(len(geojson) == 1):
                     self.logger("INFO", "geoJSON (*.geojson) found. Attempting to load.")
                     try:
-                        dta = gpd.read_file(self.zipExtractPath + geojson[0])
+                        self.geomDta = gpd.read_file(self.zipExtractPath + geojson[0])
                         self.dataLoadFail = 0
                     except Exception as e:
                         self.logger("CRITICAL", "The geojson provided failed to load. Here is what I know: " + str(e))
@@ -119,15 +130,15 @@ class builder:
     def checkSourceValidity(self):
         self.checkExistence()
         if(self.existFail == 1):
-            return("ERROR: Source file does not exist for this boundary.")
-        
-        self.metaLoad()
-        if(self.metaExistsFail == 1):
-            return("ERROR: There is no meta.txt in the source zipfile for this boundary.")
-        
+            return("INFO: Source file does not exist for this boundary.")
+
         self.unzip()
         if(self.unzip == 1):
             return("ERROR: The zipfile in the source directory failed to extract correctly.")
+
+        self.metaLoad()
+        if(self.metaExistsFail == 1):
+            return("ERROR: There is no meta.txt in the source zipfile for this boundary.")
         
         self.dataLoad()
         if(self.dataLoadFail == 1):
@@ -157,7 +168,10 @@ class builder:
                 key = "readError"
                 val = "readError"
             
-            if(("Year in key") or ("year" in key)):
+            if(("Year" in key) or ("year" in key)):
+                #pre 4.0 legacy cleanup
+                if(".0" in str(val)):
+                    val = str(val)[:-2]
                 try:
                     if "to" in val:
                         date1, date2 = val.split(" to ")
@@ -172,15 +186,15 @@ class builder:
                             self.metaReq["year"] = str(val)
                         else:
                             self.logger("CRITICAL", "The year in the meta.txt file is invalid (expected value is between 1950 and present): " + str(year))
-                            self.metaReq["year"] = "ERROR: The year provided in the metadata was invalid (Not numerically what we expected)."
+                            self.metaReq["year"] = "ERROR: The year provided in the metadata was invalid (Not numerically what we expected): " + str(year)
                 except Exception as e:
-                    self.logger("CRITICAL", "The year provided in the metadata was invalid. This is what I know:" + str(e))
+                    self.logger("CRITICAL", "The year provided in the metadata " + str(val) + " was invalid. This is what I know:" + str(e))
                     self.metaReq["year"] = "ERROR: The year provided in the metadata was invalid (Exception in log)."
 
             
             if("boundary type" in key.lower() and "name" not in key.lower()):
                 try:
-                validTypes = ["ADM0", "ADM1", "ADM2", "ADM3", "ADM4", "ADM5"]
+                    validTypes = ["ADM0", "ADM1", "ADM2", "ADM3", "ADM4", "ADM5"]
                     if(val.upper().replace(" ","") in validTypes):
                         self.logger("INFO", "Valid Boundary Type detected: " + str(val) +".")
                         self.metaReq["bType"] = val
@@ -196,7 +210,7 @@ class builder:
                 if(len(val) != 3):
                     self.logger("CRITICAL", "ISO is invalid - we expect a 3-character ISO code following ISO-3166-1 (Alpha 3).")
                     self.metaReq["iso"] = "ERROR: ISO is invalid - we expect a 3-character ISO code following ISO-3166-1 (Alpha 3)."
-                elif(val not in validISO):
+                elif(val not in self.validISO):
                     self.logger("CRITICAL", "ISO is not on our list of valid ISO-3 codes.  See https://github.com/wmgeolab/geoBoundaryBot/blob/main/dta/iso_3166_1_alpha_3.csv for all valid codes this script checks against.")
                     self.metaReq["iso"] = "ERROR: ISO is not on our list of valid ISO-3 codes."
                 else:
@@ -210,8 +224,8 @@ class builder:
                         self.logger("INFO", "Canonical name detected: " + str(val))
                         self.metaReq["canonical"] = val
                 else:
-                    self.logger("ERROR", "No canonical name detected.")
-                    self.metaReq["canonical"] = "ERROR: No canonical name detected."
+                    self.logger("WARN", "No canonical name detected.")
+                    self.metaReq["canonical"] = ""
 
 
             if("source" in key.lower() and "license" not in key.lower() and "data" not in key.lower()):
@@ -222,27 +236,30 @@ class builder:
 
 
             if("release type" in key.lower()):
-                if (val.lower() not in ["geoboundaries", "gbauthoritative", "gbhumanitarian", "UN_SALB", "UN_OCHA"]):
+                if (val.lower() not in ["geoboundaries", "gbauthoritative", "gbhumanitarian", "gbopen", "un_salb", "un_ocha"]):
                     self.logger("CRITICAL", "Invalid release type detected: " + str(val))
                     self.metaReq["releaseType"] = "ERROR: Invalid release type detected."
                 else:
-                    if(val.lower() not in z.lower()):
+                    #Legacy fixes for gb 4.0 and earlier.
+                    if(val.lower() == "gbauthoritative"):
+                        val = "UN_SALB"
+                    if(val.lower() == "gbhumanitarian"):
+                        val = "UN_OCHA"
+                    if(val.lower() == "gbopen"):
+                        val = "geoBoundaries"
+                    
+                    if(val.lower() not in self.sourcePath.lower()):
                         self.logger("CRITICAL", "ERROR: Mismatch between release type and the folder the source zip file was located in.")
-                        self.metaReq["releaseType"] = "ERROR: Mismatch between release type and the folder the source zip file was located in."
+                        self.metaReq["releaseType"] = "ERROR: Mismatch between release type and the folder the source zip file was located in: " + str(val.lower()) + " | " + str(self.sourcePath.lower())
                     else:
-                        #Legacy fixes for gb 4.0 and earlier.
-                        if(val == "gbauthoritative"):
-                            val = "UN_SALB"
-                        if(val == "gbhumanitarian"):
-                            val = "UN_OCHA"
                         self.metaReq["releaseType"]  = val.lower().strip()
 
 
 
             if("license" == key.lower()):
-                if(('"' + val.lower().strip() + '"') not in validLicense):
+                if(val.lower().strip() not in self.validLicense):
                     self.logger("CRITICAL", "Invalid license detected: " + str(val))
-                    self.metaReq["license"] = "ERROR: Invalid license detected."
+                    self.metaReq["license"] = "ERROR: Invalid license detected: " + str(val)
                 else:
                     self.metaReq["license"] = val
                     self.logger("INFO", "Valid license type detected: " + str(val))
@@ -293,3 +310,136 @@ class builder:
                 else:
                     self.logger("CRITICAL", "No license source detected.")
                     self.metaReq["licenseImage"] = "ERROR: No license source detected."
+
+
+            if("link to source data" in key.lower()):
+                if(len(val.replace(" ","")) > 0):
+                    if(val.lower() not in ["na", "nan", "null"]):
+                        self.metaReq["dataSource"] = val
+                        self.logger("INFO", "Data Source Found: " + str(val))
+                                        
+                    else:
+                        self.metaReq["dataSource"] = "ERROR: No link to source data found."
+                        self.logger("CRITICAL", "ERROR: No link to source data found.")
+
+                else:
+                    self.metaReq["dataSource"] = "ERROR: No link to source data found."
+                    self.logger("CRITICAL", "ERROR: No link to source data found.")
+
+
+            if("other notes" in key.lower()):
+                if(len(val.replace(" ","")) > 0):
+                    if(val.lower() not in ["na", "nan", "null"]):
+                        self.logger("INFO", "Other notes detected: " + val)
+                        self.metaReq["otherNotes"] = val
+                else:
+                    self.logger("WARN", "No other notes detected.  This field is optional.")
+
+        allValid = 1
+        retMes = ""
+        for d in self.metaReq:
+            if(self.metaReq[d] == "NONE"):
+                self.logger("CRITICAL",str(d) + "FAILED - NO METADATA PRESENT.")
+                retMes = retMes + " | Missing data for " + str(d) + "."
+                allValid = 0
+            
+            if("ERROR" in self.metaReq[d]):
+                self.logger("CRITICAL",str(d) + "FAILED - ERROR IN DATA." + str(self.metaReq[d]))
+                retMes = retMes + " | " + str(self.metaReq[d])
+                allValid = 0
+        
+        if(allValid == 1):
+            self.logger("INFO", "All metadata checks passed.")
+            return("Metadata checks successful")
+        else:
+            self.logger("CRITICAL", "At least one metadata check failed.")
+            return(retMes)
+    
+    def checkGeometryFiles(self):
+        self.dataLoad()
+        
+        nameC = set(['Name', 'name', 'NAME', 'shapeName', 'shapename', 'SHAPENAME']) 
+        nameCol = list(nameC & set(self.geomDta.columns))
+        if(len(nameCol) == 1):
+            self.logger("INFO", "Column for name detected: " + str(nameCol[0]))
+            try:
+                nameExample = self.geomDta[str(nameCol[0])][0]
+                nameValues = (self.geomDta[self.geomDta[str(nameCol[0])].str.contains('.*', regex=True)][str(nameCol[0])]).count()
+                self.geomReq["names"] = str(nameCol[0])
+                self.logger("INFO", "Names: " + str(nameValues) + " | Example: " + str(nameExample))
+            except Exception as e:
+                self.logger("CRITICAL", "ERROR: The file provided failed to load - it doesn't look like you have a working attribute table: " + str(e))
+                self.geomReq["names"] = "ERROR: The file provided failed to load - it doesn't look like you have a working attribute table. Check the log for the specific error."
+
+        nameIC = set(['ISO', 'ISO_code', 'ISO_Code', 'iso', 'shapeISO', 'shapeiso', 'shape_iso']) 
+        nameICol = list(nameIC & set(self.geomDta.columns))
+        if(len(nameICol) == 1):
+            self.logger("INFO", "Column for ISO detected: " + str(nameICol[0]))
+            try:
+                nameExample = self.geomDta[str(nameICol[0])][0]
+                nameValues = (self.geomDta[self.geomDta[str(nameICol[0])].str.contains('.*', regex=True)][str(nameICol[0])]).count()
+                self.geomReq["iso"] = str(nameICol[0])
+                self.logger("INFO", "ISOs: " + str(nameValues) + " | Example: " + str(nameExample))
+            except Exception as e:
+                self.logger("CRITICAL", "ERROR: The file provided failed to load - it doesn't look like you have a working attribute table: " + str(e))
+                self.geomReq["iso"] = "ERROR: The file provided failed to load - it doesn't look like you have a working attribute table. Check the log for the specific error."
+        else:
+            self.logger("WARN","No column for boundary ISOs found.  This is not required.")
+
+        for index, row in self.geomDta.iterrows():
+            xmin = row["geometry"].bounds[0]
+            ymin = row["geometry"].bounds[1]
+            xmax = row["geometry"].bounds[2]
+            ymax = row["geometry"].bounds[3]
+            tol = 1e-5
+            valid = ((xmin >= -180-tol) and (xmax <= 180+tol) and (ymin >= -90-tol) and (ymax <= 90+tol))
+            if not valid:
+                self.geomReq["bounds"] = "ERROR: At least one geometry seems to extend past the boundaries of the earth: " + str(explain_validity(row["geometry"]))
+                self.logger("CRITICAL", "ERROR: This geometry seems to extend past the boundaries of the earth: " + str(explain_validity(row["geometry"])))
+                         
+            if(not row["geometry"].is_valid):
+                self.logger("WARN", "Something is wrong with this geometry, but we might be able to fix it with a buffer: " + str(explain_validity(row["geometry"])))
+                if(not row["geometry"].buffer(0).is_valid):
+                    self.geomReq["valid"] = "ERROR: Something is wrong with this geometry, and we can't fix it: " + str(explain_validity(row["geometry"]))
+                    self.logger("CRITICAL","ERROR: Something is wrong with this geometry, and we can't fix it: " + str(explain_validity(row["geometry"])))
+                else:
+                    self.logger("WARN","A geometry error was corrected with buffer=0 in shapely.")
+
+        if("ERROR" not in self.geomReq["bounds"]):
+            self.logger("INFO","All geometries are within valid bounds.")
+            self.geomReq["bounds"] = "All geometries within valid bounds."
+
+        if("ERROR" not in self.geomReq["valid"]):
+            self.logger("INFO", "All geometries were topologically valid.")
+            self.geomReq["valid"] = "All geometries topologically valid."
+        
+        try:
+            if(self.geomDta.crs == "epsg:4326"):
+                self.logger("INFO",  "Projection confirmed as " + str(dta.crs))
+                self.geomReq["projection"] = "Projection confirmed as " + str(dta.crs)
+            else:
+                self.logger("CRITICAL",  "The projection must be EPSG 4326.  The file proposed has a projection of: " + str(dta.crs))
+                self.geomReq["projection"] = "ERROR: The projection must be EPSG 4326.  The file proposed has a projection of: " + str(dta.crs)
+        except:
+            self.logger("CRITICAL",  "The projection must be EPSG 4326.")
+            self.geomReq["projection"] = "ERROR: The projection must be EPSG 4326."
+
+        allValid = 1
+        retMes = ""
+        for d in self.geomReq:
+            if(self.geomReq[d] == "NONE"):
+                self.logger("CRITICAL",str(d) + " FAILED CHECK.")
+                retMes = retMes + " | Failed Check for " + str(d) + "."
+                allValid = 0
+            
+            if("ERROR" in self.geomReq[d]):
+                self.logger("CRITICAL",str(d) + " FAILED - ERROR IN DATA." + str(self.geomReq[d]))
+                retMes = retMes + " | " + str(self.geomReq[d])
+                allValid = 0
+        
+        if(allValid == 1):
+            self.logger("INFO", "All geometry checks passed.")
+            return("Geometry checks successful")
+        else:
+            self.logger("CRITICAL", "At least one geometry check failed.")
+            return(retMes)
