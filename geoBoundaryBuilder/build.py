bb2179bf59dd15c7ae92ebed684fe59f628d7fce
DanRunfola <danr@wm.edu>
2022-12-08 13:31:11 -0500

    Status watch

--- geoBoundaryBuilder/build.py
+++ geoBoundaryBuilder/build.py
@@ -5,6 +5,7 @@ import mpi4py
 from mpi4py import MPI
 import itertools
 import math
+import glob
 f = "/sciclone/home20/dsmillerrunfol/geoBoundaryBot/geoBoundaryBuilder/builderClass.py"
 exec(compile(open(f, "rb").read(), f, 'exec'))
 
@@ -18,6 +19,7 @@ print(comm_size)
 GB_DIR = "/sciclone/home20/dsmillerrunfol/geoBoundariesDev/"
 LOG_DIR = "/sciclone/home20/dsmillerrunfol/geoBoundaryBot/geoBoundaryBuilder/logs/"
 TMP_DIR = "/sciclone/home20/dsmillerrunfol/geoBoundariesTmp/"
+STAT_DIR = "/sciclone/home20/dsmillerrunfol/geoBoundariesStat/"
 #Limits total number of ADM units & resources requested.
 TEST = True
 
@@ -32,31 +34,48 @@ licenses = pd.read_csv("../dta/gbLicenses.csv")
 licenseList = licenses["license_name"].values
 
 if(TEST == True):
-    admTypes = ["ADM2"]
+    admTypes = ["ADM0"]
     productTypes = ["gbOpen"]
-    isoList = ["NOR"]
+    countries = pd.read_csv("../dta/iso_3166_1_alpha_3.csv")
+    isoList = countries["Alpha-3code"].values
+    #isoList = ["NOR"]
 
 if(MPI.COMM_WORLD.Get_rank() == 0):
+
+    #Clear any existing status information
+    statusFiles = glob.glob(STAT_DIR+"*")
+    for f in statusFiles:
+        os.remove(f)
 
     jobList = []
     for adm in admTypes:
         for product in productTypes:
             for iso in isoList:
                 jobList.append([iso, adm, product])
+                with open(STAT_DIR + "_" + iso + "_" + adm + "_" + product, 'w') as f:
+                    f.write("P")
                 
 
     print("Total Jobs: " + str(len(jobList)))
 
-    tasks_per_core = math.ceil(len(jobList)/ (comm_size-1))
+    #Running on 32GB machines with 10 cores each
+    #Want to ensure we have 16GB available for any single process.
+    #(2 jobs per node)
+    
+    nodeCount = comm_size/10
+    coresToAllocate = nodeCount * 2
+    
+    tasks_per_core = math.ceil(len(jobList)/ (coresToAllocate-1))
     print(tasks_per_core)
     
     taskStart = 0
     taskEnd = tasks_per_core
     for core in range(1,(comm_size-1)):
-        chunk = jobList[taskStart:taskEnd]
-        comm.send(chunk, dest=core, tag=1)
-        taskStart = taskStart + tasks_per_core
-        taskEnd = taskEnd + tasks_per_core
+        if((str(core)[-1:] == '4') or (str(core)[-1:] == '5')):
+            chunk = jobList[taskStart:taskEnd]
+            comm.send(chunk, dest=core, tag=1)
+            taskStart = taskStart + tasks_per_core
+            taskEnd = taskEnd + tasks_per_core
 
 else:
     def build(ISO, ADM, product, validISO=isoList, validLicense=licenseList):
@@ -65,31 +84,35 @@ else:
 
         bnd.checkExistence()
         if(bnd.existFail == 1):
-            return([ISO,ADM,product,"No source data for boundary exists.  Skipping."])
+            return([ISO,ADM,product,"No source data for boundary exists.  Skipping.","D"])
             
         validSource = bnd.checkSourceValidity()
         if("ERROR" in validSource):
-            return([ISO,ADM,product,validSource])
+            return([ISO,ADM,product,validSource,"V"])
 
         metaBuild = bnd.checkBuildTabularMetaData()
         if("ERROR" in metaBuild):
-            return([ISO,ADM,product,metaBuild])
+            return([ISO,ADM,product,metaBuild,"M"])
 
         geomChecks = bnd.checkBuildGeometryFiles()
         if("ERROR" in geomChecks):
-            return([ISO,ADM,product,geomChecks])
+            return([ISO,ADM,product,geomChecks,"G"])
 
         saveFiles = bnd.constructFiles()
         if("ERROR" in saveFiles):
-            return([ISO, ADM, product, saveFiles])
+            return([ISO, ADM, product, saveFiles,"C"])
 
         
-        return([ISO,ADM,product,"Succesfully built."])
+        return([ISO,ADM,product,"Succesfully built.","D"])
 
     layers = comm.recv(source=0, tag=1)
     print("I (" + str(MPI.COMM_WORLD.Get_rank()) + ") have received " + str(layers) + " layers to build.")
     ret = []
     for l in layers:
-        ret.append(build(ISO=l[0], ADM=l[1], product=l[2]))
+        o = build(ISO=l[0], ADM=l[1], product=l[2])
+        ret.append(o)
+        with open(STAT_DIR + "_" + l[0] + "_" + l[1] + "_" + l[2], 'w') as f:
+            f.write(o[4])
+
     
     print(ret)
---
+++ geoBoundaryBuilder/watch.py
@@ -0,0 +1,83 @@
+#source "/usr/local/anaconda3-2021.05/etc/profile.d/conda.csh"
+#module load anaconda3/2021.05
+#conda activate geoBoundariesBuild
+
+import pandas as pd
+import glob
+
+STAT_DIR = "/sciclone/home20/dsmillerrunfol/geoBoundariesStat/"
+statusFiles = glob.glob(STAT_DIR+"*")
+
+statList = {}
+
+for f in statusFiles:
+    with open(f,"r") as fHandler:
+        v = fHandler.read()
+        iso = str(f).split("_")[1]
+        adm = str(f).split("_")[2]
+        product = str(f).split("_")[3]
+        
+        key = iso + "_" + product
+
+        if(key not in statList.keys()):
+            statList[key] = ["", "|","|","|","|","|","|"]
+        
+        if(adm == "ADM0"):
+            statList[key][1] = v
+        if(adm == "ADM1"):
+            statList[key][2] = v
+        if(adm == "ADM2"):
+            statList[key][3] = v
+        if(adm == "ADM3"):
+            statList[key][4] = v
+        if(adm == "ADM4"):
+            statList[key][5] = v
+        if(adm == "ADM5"):
+            statList[key][6] = v
+        
+        statList[key][0] = key
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+headers = ["LAYER", "A0","A1","A2","A3","A4","A5"]
+count = 0
+for h in headers:
+    if (count == 0):
+        print('{0:<15}'.format(h), end='')
+        count = count + 1
+    else:
+        print('{0:<3}'.format(h), end='')
+    
+print()
+rowCount = 0
+for dta in statList:
+    colCount = 0
+    for adm in statList[dta]:
+        if(colCount == 0):
+            print(bcolors.HEADER + '{0:<15}'.format(adm), end='')
+            colCount = colCount + 1
+        else:
+            if(adm == "D"):
+                print(bcolors.OKGREEN + '{0:<3}'.format(adm), end='')
+            elif(adm == "|"):
+                print(bcolors.OKBLUE + '{0:<3}'.format(adm), end='')
+            elif(adm == "P"):
+                print(bcolors.WARNING + '{0:<3}'.format(adm), end='')
+            else:
+                print(bcolors.FAIL + '{0:<3}'.format(adm), end='')
+    
+    if(rowCount == 0 or rowCount == 1 or rowCount == 2 or rowCount == 3):
+        rowCount = rowCount + 1
+    else:
+        rowCount = 0
+        print()
+    #print()
